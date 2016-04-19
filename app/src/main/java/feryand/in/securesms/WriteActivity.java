bce9e22568271f8c2259b517dd364121a7a817ba
feryandi <feryandi.n@gmail.com>
2016-04-20 01:52:36 +0700

    Checkbox Functionality

--- app/src/main/java/feryand/in/securesms/WriteActivity.java
+++ app/src/main/java/feryand/in/securesms/WriteActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.Toolbar;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 import java.util.ArrayList;
@@ -20,6 +21,11 @@ public class WriteActivity extends AppCompatActivity {
     Button sendPlain;
     EditText receiver;
     EditText message;
+
+    CheckBox E;
+    EditText EK;
+    CheckBox C;
+
     DBHandler dbHandler = new DBHandler(this, null, null, 1);
 
     @Override
@@ -34,6 +40,10 @@ public class WriteActivity extends AppCompatActivity {
         receiver = (EditText) findViewById(R.id.receiver);
         message = (EditText) findViewById(R.id.message);
 
+        E = (CheckBox) findViewById(R.id.encryption);
+        EK = (EditText) findViewById(R.id.encKey);
+        C = (CheckBox) findViewById(R.id.signature);
+
         sendPlain.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 sendSMSMessage(view);
@@ -45,14 +55,30 @@ public class WriteActivity extends AppCompatActivity {
         String r = receiver.getText().toString();
         String m = message.getText().toString();
 
-        ECDSA ec = new ECDSA();
-        Data dataPri = dbHandler.findData("pri");
-        ec.setPri(dataPri.getValue());
+        String option = "";
 
-        SHA1 s = new SHA1(m);
-        m = "<ss>" + "DC" + "</ss>" + m;
-        Point rs = ec.generateSignature(s.getDigest());
-        m += "<ds>04" + (rs.getX()).toString(16) + "" + (rs.getY()).toString(16) + "</ds>";
+        if ( E.isChecked() ) {
+            option += "E";
+        } else {
+            option += "D";
+        }
+
+        if ( C.isChecked() ) {
+            option += "C";
+        } else {
+            option += "N";
+        }
+
+        m = "<ss>" + option + "</ss>" + m;
+
+        if ( C.isChecked() ) {
+            ECDSA ec = new ECDSA();
+            Data dataPri = dbHandler.findData("pri");
+            ec.setPri(dataPri.getValue());
+            SHA1 s = new SHA1(m);
+            Point rs = ec.generateSignature(s.getDigest());
+            m += "<ds>04" + (rs.getX()).toString(16) + "" + (rs.getY()).toString(16) + "</ds>";
+        }
 
         try {
             SmsManager smsManager = SmsManager.getDefault();
@@ -64,7 +90,7 @@ public class WriteActivity extends AppCompatActivity {
             Snackbar.make(v, "Your message already sent.", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show();
 
-            finish();
+            //finish();
         } catch (Exception e) {
             Snackbar.make(v, "We cannot send the message. Please try again. \n Error: " + e, Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show();
