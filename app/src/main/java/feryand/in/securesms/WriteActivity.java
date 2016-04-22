13be461affb0a861465d2523b09804b25061a470
Yoga Adrian <yoga.adrians14@gmail.com>
2016-04-22 14:40:44 +0700

    au

--- app/src/main/java/feryand/in/securesms/WriteActivity.java
+++ app/src/main/java/feryand/in/securesms/WriteActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
